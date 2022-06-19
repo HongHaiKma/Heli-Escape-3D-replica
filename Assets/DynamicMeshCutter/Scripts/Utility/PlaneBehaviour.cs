9f9da3eb7de641e1e6583f07082943d8f3fa2844
Hong Hai Nguyen <nguyenhonghailqd1996@gmail.com>
2022-06-19 15:54:09 +0700

    no message

--- Assets/DynamicMeshCutter/Scripts/Utility/PlaneBehaviour.cs
+++ Assets/DynamicMeshCutter/Scripts/Utility/PlaneBehaviour.cs
@@ -1,8 +1,11 @@
 
+using Exploder.Utils;
+
 namespace DynamicMeshCutter
 {
     public class PlaneBehaviour : CutterBehaviour
     {
+        public MeshTarget[] m_MeshTarget;
         public float DebugPlaneLength = 2;
         public void Cut()
         {
@@ -10,11 +13,26 @@ namespace DynamicMeshCutter
             foreach (var root in roots)
             {
                 if (!root.activeInHierarchy)
-                    continue;
-                var targets = root.GetComponentsInChildren<MeshTarget>();
-                foreach (var target in targets)
                 {
-                    Cut(target, transform.position, transform.forward, null, OnCreated);
+                    continue;
+                }
+                else
+                {
+                    Helper.DebugLog("Name Root: " + root.name);
+                }
+                    
+                // var targets = root.GetComponentsInChildren<MeshTarget>();
+                // foreach (var target in targets)
+                // {
+                //     Helper.DebugLog("Name: " + target.name);
+                //     Cut(target, transform.position, transform.forward, null, OnCreated);
+                //     ExploderSingleton.Instance.ExplodeObject(target.gameObject);
+                // }
+
+                for (int i = 0; i < m_MeshTarget.Length; i++)
+                {
+                    Cut(m_MeshTarget[i], transform.position, transform.forward, null, OnCreated);
+                    ExploderSingleton.Instance.ExplodeObject(m_MeshTarget[i].gameObject);
                 }
             }
         }
